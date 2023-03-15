059cfbe88cfb8854b7a982c7dcc12612cde1eb7a
ThanhSonTran <thanhsontran127@gmail.com>
2023-03-15 23:29:58 +0700

    custom layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/component/Layout';
+
 function App() {
     return (
-        <div className="app">
-            <h1>hello work</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={route.path}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
