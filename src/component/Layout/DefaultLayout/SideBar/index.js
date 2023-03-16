6b7a013e65d716a8c27d6fba9b5b12f2ad270c23
ThanhSonTran <thanhsontran127@gmail.com>
2023-03-16 23:43:33 +0700

    set DefaultLayout

--- src/component/Layout/DefaultLayout/SideBar/index.js
+++ src/component/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return <aside>
+        <div className={cx('wrapper')}>
+            <h2>SideBar</h2>
+        </div>
+    </aside>
 }
 
 export default Sidebar;
